5f05febaad25adcac00282378e22cb3fc0541a06
DESKTOP-JVKHS7I\Adam <adsau59@gmail.com>
2020-01-31 17:23:16 +0530

    checker system made

--- svadder.py
+++ svadder.py
@@ -1,7 +1,12 @@
+import functools
 import math
+import subprocess
+import threading
+import time
 import win32gui
 from win32api import GetSystemMetrics
 
+import waiting
 from PIL import ImageGrab
 import numpy as np
 
@@ -9,18 +14,36 @@ import os
 
 import cv2
 import matplotlib
-from gtts import gTTS
 from pynput.keyboard import Key, Listener
 from pytesseract import pytesseract
 
 import matplotlib.pyplot as plt
-from playsound import playsound
 
 img = None
 run = True
-update = False
+show_new_image_flag = False
+debug = False
+old_caught = False
 mp3_name = 'temp.mp3'
-x, y0, dy, w, h, n = 595, 513, 60, 40, 22, 7
+espeak_loc = r'C:\Program Files (x86)\eSpeak\command_line\espeak.exe'
+
+# x, y0, dy, w, h, n = 595, 513, 60, 40, 22, 7
+x, y0, dy, w, h, n = 1463, 376, 39, 38, 19, 7
+caught_data = [(1476, 347, 100, 110), (1183, 801, 27, 32)]  # x, y, h0, h1
+
+
+def toggle_debug():
+    global debug
+    debug = not debug
+
+
+def turn_off():
+    global run
+    run = False
+
+
+def tts(text):
+    subprocess.Popen([espeak_loc, text])
 
 
 def get_game_screenshot():
@@ -47,6 +70,11 @@ def get_game_screenshot():
     return tempScreen[crop[1]:crop[3], crop[0]:crop[2]]
 
 
+def process_img(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    return img[:, :, 2]
+
+
 def mypause(interval):
     backend = plt.rcParams['backend']
     if backend in matplotlib.rcsetup.interactive_bk:
@@ -59,52 +87,92 @@ def mypause(interval):
             return
 
 
+def crop_img(i):
+    y = y0 + (dy * i)
+    img2 = img[y:y + h, x:x + w]
+    return img2
+
+
+# noinspection PyBroadException
+def read_sv(img2):
+    try:
+        ret, img2 = cv2.threshold(img2, 220, 255, cv2.THRESH_BINARY_INV)
+        img2 = cv2.resize(img2, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+        text = pytesseract.image_to_string(img2, config='--psm 8 --oem 0 -c tessedit_char_whitelist=0123456789')
+        return int(text)
+    except Exception:
+        print("there was en error reading svs")
+        return 0
+
+
+def tell_sv():
+    global img, run, show_new_image_flag
+
+    print("checking SV")
+    start = time.time()
+
+    img = process_img(get_game_screenshot())
+
+    sv_cropped = [crop_img(x) for x in range(n)]
+
+    if debug:
+        for i in sv_cropped:
+            show_image(i)
+            waiting.wait(lambda: not show_new_image_flag)
+
+    result = [read_sv(x) for x in sv_cropped]
+    s = sum(result)
+
+    print(f"took {round(time.time() - start, 2)}s")
+    tts(f"'Total SV is {s}'")
+
+
+def show_image(to_show_img):
+    global img, show_new_image_flag
+
+    img = to_show_img
+    show_new_image_flag = True
+
+
 def on_release(key):
-    global img, run, update
+    global show_new_image_flag
 
+    if key == Key.f8:
+        toggle_debug()
+        print(f"Debug is turned {'on' if debug else 'off'}")
     if key == Key.f9:
-        img = get_game_screenshot()
-
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        img = img[:, :, 2]
-        print(img.shape)
-        img2 = img
-
-        s = 0
-        for i in range(n):
-            y = y0 + (dy * i)
-            img2 = img[y:y + h, x:x + w]
-            ret, img2 = cv2.threshold(img2, 220, 255, cv2.THRESH_BINARY_INV)
-            img2 = cv2.resize(img2, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-            text = pytesseract.image_to_string(img2, config='--psm 8 --oem 0 -c tessedit_char_whitelist=0123456789')
-
-            try:
-                s += int(text)
-            except Exception:
-                pass
-
-        sound = gTTS(f'Total SV is {s}', lang='en', slow=False)
-
-        if os.path.exists(mp3_name):
-            os.remove(mp3_name)
-        sound.save(mp3_name)
-        playsound(mp3_name)
-
-        update = True
+        tell_sv()
+    elif key == Key.f10:
+        show_image(cv2.cvtColor(get_game_screenshot(), cv2.COLOR_BGR2HSV))
     elif key == Key.f11:
-        run = False
+        turn_off()
+
+
+def start_check_for_capture():
+    global old_caught
+
+    while run:
+        im = cv2.cvtColor(get_game_screenshot(), cv2.COLOR_BGR2HSV)
+        caught = functools.reduce(lambda a, b: a and b[2] < im[b[1]][b[0]][0] < b[3], caught_data, True)
+        if caught and not old_caught:
+            tts("Please wait while I calculate total SV")
+            tell_sv()
+
+        old_caught = caught
+        time.sleep(.5)
 
 
 if __name__ == "__main__":
+    checker = threading.Thread(target=start_check_for_capture)
+    checker.start()
+
     with Listener(
             on_release=on_release) as listener:
-        listener.join()
+        print("ready")
+        while run:
+            waiting.wait(lambda: show_new_image_flag)
+            plt.imshow(img, cmap='gray')
+            plt.show()
+            show_new_image_flag = False
 
-        # while run:
-        #     if img is not None:
-        #         if update:
-        #             im = plt.imshow(img, cmap='gray')
-        #             plt.ion()
-        #             plt.show()
-        #             update = False
-        #         mypause(0.01)
+    checker.join()
