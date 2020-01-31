ca5ae6a4704a28fb7edd526e3fec2988837ed8be
DESKTOP-JVKHS7I\Adam <adsau59@gmail.com>
2020-01-31 18:05:55 +0530

    cleaned code, made pixel value dynamic

--- svadder.py
+++ svadder.py
@@ -10,64 +10,62 @@ import waiting
 from PIL import ImageGrab
 import numpy as np
 
-import os
-
 import cv2
-import matplotlib
 from pynput.keyboard import Key, Listener
 from pytesseract import pytesseract
 
 import matplotlib.pyplot as plt
 
-img = None
-run = True
-show_new_image_flag = False
-debug = False
-old_caught = False
-mp3_name = 'temp.mp3'
 espeak_loc = r'C:\Program Files (x86)\eSpeak\command_line\espeak.exe'
-
-# x, y0, dy, w, h, n = 595, 513, 60, 40, 22, 7
-x, y0, dy, w, h, n = 1463, 376, 39, 38, 19, 7
-caught_data = [(1476, 347, 100, 110), (1183, 801, 27, 32)]  # x, y, h0, h1
+full_screen = True
+debug = False
 
 
-def toggle_debug():
-    global debug
-    debug = not debug
+class G:
+    img = None
+    run = True
+    show_new_image_flag = False
+    old_caught = False
 
 
 def turn_off():
-    global run
-    run = False
+    G.run = False
 
 
 def tts(text):
     subprocess.Popen([espeak_loc, text])
 
 
+def show_image(to_show_img):
+    G.img = to_show_img
+    G.show_new_image_flag = True
+
+
 def get_game_screenshot():
-    # initialization
     hwnd = win32gui.FindWindow(None, "Temtem")
     rect = win32gui.GetWindowRect(hwnd)
-    clientRect = win32gui.GetClientRect(hwnd)
-    windowOffset = math.floor(((rect[2] - rect[0]) - clientRect[2]) / 2)
+    client_rect = win32gui.GetClientRect(hwnd)
+    window_offset = math.floor(((rect[2] - rect[0]) - client_rect[2]) / 2)
 
-    # titleOffset = ((rect[3] - rect[1]) - clientRect[3]) - windowOffset
-    titleOffset = 0
+    title_offset = 0 if full_screen else ((rect[3] - rect[1]) - client_rect[3]) - window_offset
 
-    # saving the image
     bbox = (0, 0, GetSystemMetrics(0), GetSystemMetrics(1))
 
-    tempScreen = np.array(ImageGrab.grab(bbox=bbox))
-
-    # tempScreen = cv2.cvtColor(tempScreen, cv2.COLOR_BGR2RGB)
+    temp_screen = np.array(ImageGrab.grab(bbox=bbox))
 
     rect = win32gui.GetWindowRect(hwnd)
-    crop = (rect[0] + windowOffset, rect[1] + titleOffset, rect[2] - windowOffset,
-            rect[3] - windowOffset)
+    crop = (rect[0] + window_offset, rect[1] + title_offset, rect[2] - window_offset,
+            rect[3] - window_offset)
 
-    return tempScreen[crop[1]:crop[3], crop[0]:crop[2]]
+    return temp_screen[crop[1]:crop[3], crop[0]:crop[2]]
+
+
+X0, Y0 = 1920, 1080
+Y, X, _ = get_game_screenshot().shape
+# x, y0, dy, w, h, n = 595, 513, 60, 40, 22, 7
+x, y0, dy, w, h, n = int(1463 * X / X0), int(376 * Y / Y0), int(39 * Y / Y0), int(38 * X / X0), int(19 * Y / Y0), 7
+caught_data = [(int(1476 * X / X0), int(347 * Y / Y0), 100, 110),
+               (int(1183 * X / X0), int(801 * Y / Y0), 27, 32)]  # x, y, h0, h1
 
 
 def process_img(img):
@@ -75,21 +73,9 @@ def process_img(img):
     return img[:, :, 2]
 
 
-def mypause(interval):
-    backend = plt.rcParams['backend']
-    if backend in matplotlib.rcsetup.interactive_bk:
-        figManager = matplotlib._pylab_helpers.Gcf.get_active()
-        if figManager is not None:
-            canvas = figManager.canvas
-            if canvas.figure.stale:
-                canvas.draw()
-            canvas.start_event_loop(interval)
-            return
-
-
-def crop_img(i):
+def crop_img(image, i):
     y = y0 + (dy * i)
-    img2 = img[y:y + h, x:x + w]
+    img2 = image[y:y + h, x:x + w]
     return img2
 
 
@@ -106,14 +92,12 @@ def read_sv(img2):
 
 
 def tell_sv():
-    global img, run, show_new_image_flag
-
     print("checking SV")
     start = time.time()
 
-    img = process_img(get_game_screenshot())
+    image = process_img(get_game_screenshot())
 
-    sv_cropped = [crop_img(x) for x in range(n)]
+    sv_cropped = [crop_img(image, x) for x in range(n)]
 
     if debug:
         for i in sv_cropped:
@@ -127,19 +111,7 @@ def tell_sv():
     tts(f"'Total SV is {s}'")
 
 
-def show_image(to_show_img):
-    global img, show_new_image_flag
-
-    img = to_show_img
-    show_new_image_flag = True
-
-
 def on_release(key):
-    global show_new_image_flag
-
-    if key == Key.f8:
-        toggle_debug()
-        print(f"Debug is turned {'on' if debug else 'off'}")
     if key == Key.f9:
         tell_sv()
     elif key == Key.f10:
@@ -149,30 +121,30 @@ def on_release(key):
 
 
 def start_check_for_capture():
-    global old_caught
-
-    while run:
+    while G.run:
         im = cv2.cvtColor(get_game_screenshot(), cv2.COLOR_BGR2HSV)
         caught = functools.reduce(lambda a, b: a and b[2] < im[b[1]][b[0]][0] < b[3], caught_data, True)
-        if caught and not old_caught:
+        if caught and not G.old_caught:
             tts("Please wait while I calculate total SV")
             tell_sv()
 
-        old_caught = caught
+        G.old_caught = caught
         time.sleep(.5)
 
 
 if __name__ == "__main__":
+
     checker = threading.Thread(target=start_check_for_capture)
     checker.start()
 
     with Listener(
             on_release=on_release) as listener:
-        print("ready")
-        while run:
-            waiting.wait(lambda: show_new_image_flag)
-            plt.imshow(img, cmap='gray')
-            plt.show()
-            show_new_image_flag = False
+        print("When you capture a Temtem I'll calculate its total SV\nPress f11 to quit")
+        while G.run:
+            waiting.wait(lambda: G.show_new_image_flag or not G.run)
+            if G.img is not None:
+                plt.imshow(G.img, cmap='gray')
+                plt.show()
+                show_new_image_flag = False
 
     checker.join()
